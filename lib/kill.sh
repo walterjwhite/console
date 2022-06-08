27b8d92c143b27601cae2ba28c55159816f65d3f
Walter White <WalterWhite@walterjwhite.com>
2022-06-08 00:00:00 -0400

    merged

--- lib/kill.sh
+++ lib/kill.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
+. _LIBRARY_PATH_/console/sed.sh
+
+_kill_all() {
+	for _EXISTING_APPLICATION_PIPE in $(find /tmp/$_APPLICATION_NAME -type p -not -name $$); do
+		_kill $(basename $_EXISTING_APPLICATION_PIPE)
+	done
+}
+
 _kill() {
 	_warn "Killing $1"
 	kill -TERM $1
 }
 
+_list() {
+	_info "Running processes:"
+
+	_EXECUTABLE_NAME_SED_SAFE=$(_sed_safe $0)
+
+	for _EXISTING_APPLICATION_PIPE in $(find /tmp/$_APPLICATION_NAME -type p -not -name $$); do
+		_TARGET_PID=$(basename $_EXISTING_APPLICATION_PIPE)
+		_TARGET_PS_DTL=$(ps -o command -p $_TARGET_PID | sed 1d | sed -e "s/^.*$_EXECUTABLE_NAME_SED_SAFE/$_EXECUTABLE_NAME_SED_SAFE/")
+
+		_info "\t$_TARGET_PID - $_TARGET_PS_DTL"
+	done
+}
+
 for _ARG in "$@"; do
 	case "$_ARG" in
 	-kill-all)
-		for _EXISTING_APPLICATION_PIPE in $(find /tmp/$_APPLICATION_NAME -type p -not -name $$); do
-			_kill $(basename $_EXISTING_APPLICATION_PIPE)
-		done
+		_kill_all
 
 		_exitSuccess "Killed"
 		;;
@@ -18,6 +37,11 @@ for _ARG in "$@"; do
 		_kill ${_ARG#*=}
 
 		_exitSuccess "Killed"
+		;;
+	-l)
+		_list
+
+		_exitSuccess "listed running processes"
 		;;
 	esac
 done
